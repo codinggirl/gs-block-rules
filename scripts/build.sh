b18108be67a937168c4a73eaeb8cd698350f4188
Richard Libre <meng@codinggirl.net>
2019-05-30 14:35:56 +0800

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,7 @@
 mkdir -p dist
 
 # uBlocklist file
-cat data/pattern-and-re.txt | sort > dist/uBlacklist.txt
+cat data/pattern-and-re.txt | sort | uniq > dist/uBlacklist.txt
+
+# sort url
+cat data/url.txt | sort | uniq > dist/url.txt
